7de07440dd9da2f4df40611a89339e7fbfae4c45
Aida <sultanovaaida29@gmail.com>
2023-10-10 17:50:18 +0600

    hw2

---
+++ app.js
@@ -0,0 +1,15 @@
+var starbucks = {
+    coffee: {
+        latte: "one or two shots of espresso, steamed milk and a final, thin layer of frothed milk on top",
+        arabica: ' This coffee is cost $13',
+        cappuccino: 'This coffee is cost $12',
+        americano: 'This coffee is cost $16',
+        espresso: 'This coffee is cost $14'
+    },
+    tea: {
+        GreenTea: 'This tea is cost $10',
+        BlackTea: 'This tea is cost $24',
+        TeaWithLemonade: 'This tea is cost $5',
+        Matcha: 'This tea is cost $12'
+    }
+}
---
+++ dop-1/app.js
@@ -0,0 +1,26 @@
+var num1 = prompt('enter number')
+switch (num1){
+    case num1 = '1':
+        console.log('monday')
+        break;
+    case num1 = '2':
+        console.log('tuesday')
+        break;
+    case num1 = '3':
+        console.log('wednesday')
+        break;
+    case num1 = '4':
+        console.log('thursday')
+        break;
+    case num1 = '5':
+        console.log('friday')
+        break;
+    case num1 = '6':
+        console.log('saturday')
+        break;
+    case num1 = '7':
+        console.log('sunday')
+        break;
+    default:
+        console.log('enter a number from 1 to 7')
+}
